ee13775046dc4f48eccc96817e16c01359dfe286
lfq <lfqiang2019@gmail.com>
2026-03-15 18:28:33 +0800

    revert: add service worker info

--- src/main.js
+++ src/main.js
@@ -13,38 +13,3 @@ app.use(router)
 app.use(ElementPlus)
 
 app.mount('#app')
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw.js', { scope: '/' })
-      .then((registration) => {
-        if (registration.waiting) {
-          registration.waiting.postMessage({ type: 'GET_SW_INFO' })
-        }
-        if (registration.active) {
-          registration.active.postMessage({ type: 'GET_SW_INFO' })
-        }
-        registration.addEventListener('updatefound', () => {
-          const installing = registration.installing
-          if (installing) {
-            installing.addEventListener('statechange', () => {
-              if (installing.state === 'installed' && navigator.serviceWorker.controller) {
-                console.log('Service worker updated')
-              }
-            })
-          }
-        })
-      })
-      .catch((error) => {
-        console.error('Service worker registration failed', error)
-      })
-  })
-
-  navigator.serviceWorker.addEventListener('message', (event) => {
-    if (event.data && event.data.type === 'SW_INFO') {
-      const { version, scope, cacheName, enabledCache } = event.data
-      console.log('Service worker info', { version, scope, cacheName, enabledCache })
-    }
-  })
-}
